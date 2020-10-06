06ae640ce83bafb950c17584cc8f0eecea3ee8f8
Thomas Brookshaw <tbrookshaw13@gmail.com>
2020-10-06 17:03:30 -0500

    notes finish

---
+++ server/server.js
@@ -0,0 +1,35 @@
+// Require express - gives us a function
+const express = require('express');
+
+// create an instance of express by calling the function
+// returned above - gives us an object
+const app = express();
+const port = 5000;
+
+// express static file serving - public is the folder name
+app.use(express.static('server/public'));
+
+let quotesData = [
+    { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+    { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+    { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+];
+
+let index = 0
+
+app.get('/quotes', (req, res) => {
+    console.log('Hi from git request')
+    res.send(quotesData);
+});
+
+app.get('/randomQuote', (req, res) => {
+    console.log('Hi from git request')
+    let randomNumber = getRandomInt(quotesData.length);
+    res.send(quotesData[randomNumber]);
+});
+
+app.listen(port, () => {
+    console.log('Up and running on port: ', port);
+});
+
+
